aad8e8ece6acf935d161b881ad58b5dca9ba21b7
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-01-27 19:12:34 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 echo "Running Fluxpipe"
 echo $1
 echo 'result<<EOF' >> $GITHUB_OUTPUT
-echo $1 | /fluxpipe-server -stdin >> $GITHUB_OUTPUT
+echo $1 | /fluxpipe-server -stdin | /c2j >> $GITHUB_OUTPUT
 echo 'EOF' >> $GITHUB_OUTPUT
