ed54ea4e403eb92c074ab958c4d81376a607c1c0
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-01-27 12:23:25 +0100

    Update start-fluxpipe.sh

--- start-fluxpipe.sh
+++ start-fluxpipe.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker run --name fluxpipe -p 8086:8086 -d ghcr.io/metrico/fluxpipe:latest /fluxpipe-server -port 8086
+docker run --name fluxpipe -p 8086:8086 -d ghcr.io/metrico/fluxpipe:latest
+docker ps
+echo "FluxPipe Running"
